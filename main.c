5aca5f7282dd5835e223db7ec5a5632f02e6d200
brendisurfs <brendan.prednis@protonmail.com>
2021-10-19 21:52:53 -0700

    init commit: Learning C through writing a basic shell.

---
+++ main.c
@@ -0,0 +1,46 @@
+/**
+ * @file main.c
+ * @author Brendan Prednis
+ * @brief
+ * @version 0.1
+ * @date 2021-10-19
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * BASIC PROGRAM LOGIC
+ Read: Read the command from stdin.
+ Parse: Seperate the command string into a program and arguments.
+ Execute: Run the parsed command.
+ *
+ */
+void bsh_loop(void) {
+      char *line;
+      char **args;
+      int status;
+
+      do {
+            printf("> ");
+            line = bsh_read_line();
+            args = bsh_split_line(line);
+            status = bsh_execute(args);
+
+            free(line);
+            free(args);
+      } while (status);
+}
+
+int main(int argc, char **argv) {
+      // 1. load any config files
+
+      // 2. Run command loop.
+      bsh_loop();
+
+      //   3. Perform any shutdown/cleanup.
+      return EXIT_SUCCESS;
+}
